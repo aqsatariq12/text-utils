dcd0cccdd13e6191b0deec6eb720f15342244926
Aqsa tariq <aqsatariq2452003@gmail.com>
2025-02-16 00:45:31 -0800

    Fix: Resolved merge conflicts in Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,30 +1,17 @@
-import React from 'react'
+import React from 'react';  // Only once at the top
 
 export default function Alert(props) {
-    const capitalize = (word)=>{
+    const capitalize = (word) => {
         const lower = word.toLowerCase(); 
-        return lower.charAt(0).toUpperCase()+ lower.slice(1);
+        return lower.charAt(0).toUpperCase() + lower.slice(1);
     }
-  return (
-    props.alert && <div>
-      <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-            <strong>{capitalize(props.alert.type)}</strong>:{props.alert.msg}
-        </div>
-    </div>
-  )
-}
-import React from 'react'
 
-export default function Alert(props) {
-    const capitalize = (word)=>{
-        const lower = word.toLowerCase(); 
-        return lower.charAt(0).toUpperCase()+ lower.slice(1);
-    }
-  return (
-    props.alert && <div>
-      <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-            <strong>{capitalize(props.alert.type)}</strong>:{props.alert.msg}
+    return (
+        props.alert && 
+        <div>
+            <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+                <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
+            </div>
         </div>
-    </div>
-  )
+    );
 }
