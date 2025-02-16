bd7e26a057095eb389dcf843c6c54d4ae6b075bb
Aqsa tariq <aqsatariq2452003@gmail.com>
2025-02-16 00:17:48 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import "./App.css"; //can be changed
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
-<<<<<<< HEAD
+
 // import About from "./components/About";
 import React, { useState } from "react";
 
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // React Router v6
-=======
 import About from "./components/About";
 import React, { useState } from "react";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // React Router v6
->>>>>>> a83e908 (Initial commit)
+// import About from "./components/About";
+import React, { useState } from "react";
 
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // React Router v6
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -43,7 +44,6 @@ function App() {
     }, 1500)
   }
   return (
-<<<<<<< HEAD
     // <Router>
     //   <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} aboutText="About"/>
     //   <Alert alert={alert} />
@@ -62,18 +62,16 @@ function App() {
     <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
     </div>
     </>
-=======
-    <Router>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} aboutText="About"/>
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <Routes>
-          <Route exact path="/about" element={<About />} />
-          <Route eaxct path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />} />
-        </Routes>
-      </div>
-    </Router>
->>>>>>> a83e908 (Initial commit)
+    // <Router>
+    //   <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} aboutText="About"/>
+    //   <Alert alert={alert} />
+    //   <div className="container my-3">
+    //     <Routes>
+    //       <Route exact path="/about" element={<About />} />
+    //       <Route eaxct path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />} />
+    //     </Routes>
+    //   </div>
+    // </Router>
   );
 }
 
